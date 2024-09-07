412c8dbea8bc252cd6740f212afc1422369e793c
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-09-07 22:49:58 +0500

    api in installed apps

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api.apps.ApiConfig',
     'rest_framework.authtoken',
     'posts.apps.PostsConfig',
 ]
