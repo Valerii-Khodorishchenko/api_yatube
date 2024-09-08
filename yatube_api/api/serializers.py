e32c3303dde5bc33f50849ed5e391684076bea2c
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-09-08 21:00:40 +0500

    Fixed: Only authorized author can delete and edit posts

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from posts.models import Post
+from posts.models import Comment, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.PrimaryKeyRelatedField(
+    #     read_only=True, default=serializers.CurrentUserDefault()
+    # )
+    author = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Post
         fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
 
 from .serializers import Post, PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def destroy(self, request, *args, **kwargs):
+        post = self.get_object()
+        if post.author != request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        return super().destroy(request, *args, **kwargs)
+
