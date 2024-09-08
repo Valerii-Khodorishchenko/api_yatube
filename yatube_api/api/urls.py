afc3f3d18f390e3efd2e236c16212779cff51ebc
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-09-08 17:47:25 +0500

    added path, serializer, viewset for Post model

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
 
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
-    # path()
+    path('', include(router.urls))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import Post, PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
